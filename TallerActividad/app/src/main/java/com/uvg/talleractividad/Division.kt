ef7c23ed5fc99bf93ace3c775ba9103ee7e6ba0b
Racso1624 <fer10_lb@outlook.com>
2021-05-22 21:57:52 -0600

    Se arregla el programa

---
+++ TallerActividad/app/src/main/java/com/uvg/talleractividad/Division.kt
@@ -0,0 +1,45 @@
+package com.uvg.talleractividad
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import kotlinx.android.synthetic.main.division.*
+import java.lang.Exception
+
+class Division : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.division)
+
+        btnRegresardiv.setOnClickListener{
+            var numero1:Double = 0.0
+            var numero2:Double = 0.0
+
+            val valor1: EditText = findViewById(R.id.valordiv1)
+            val valor2: EditText = findViewById(R.id.valordiv2)
+
+            val texto1:String = valor1.text.toString()
+            val texto2:String = valor2.text.toString()
+
+            try {
+                numero1 = texto1.toDouble()
+                numero2 = texto2.toDouble()
+            }
+            catch (e: Exception){
+                Toast.makeText(this, "Error, Ingrese un Numero", Toast.LENGTH_SHORT).show()
+            }
+
+            val resultado:Double = numero1 / numero2
+
+            val textoResultado:String = resultado.toString()
+
+            val intent: Intent = Intent(this,MainActivity::class.java)
+            intent.putExtra("resultado",textoResultado)
+            setResult(RESULT_OK,intent)
+            finish()
+        }
+
+    }
+}
--- TallerActividad/app/src/main/java/com/uvg/talleractividad/MainActivity.kt
+++ TallerActividad/app/src/main/java/com/uvg/talleractividad/MainActivity.kt
@@ -4,10 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,121 +13,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnSuma.setOnClickListener{
-            val intent: Intent = Intent(this, MainActivity2::class.java)
-
-            var numero1: Int = 0
-            var numero2: Int = 0
-            var resultado:Float = 0f
-
-            val valor1:EditText = findViewById(R.id.valor1)
-            val valor2:EditText = findViewById(R.id.valor2)
-
-            val texto1:String = valor1.text.toString()
-            val texto2:String = valor2.text.toString()
-
-            try {
-                numero1 = texto1.toInt()
-                numero2 = texto2.toInt()
-            }
-            catch (e:InputMismatchException){
-                Toast.makeText(this, "Error, Ingrese un Numero", Toast.LENGTH_SHORT).show()
-            }
-
-            resultado = (numero1 + numero2).toFloat()
-
-            val textoresultado = resultado.toString()
-
-            intent.putExtra("resultado", textoresultado)
+            val intent: Intent = Intent(this, Suma::class.java)
 
             startActivityForResult(intent, 1)
         }
 
         btnResta.setOnClickListener{
-            val intent: Intent = Intent(this, MainActivity2::class.java)
-
-            var numero1: Int = 0
-            var numero2: Int = 0
-            var resultado:Float = 0f
-
-            val valor1:EditText = findViewById(R.id.valor1)
-            val valor2:EditText = findViewById(R.id.valor2)
-
-            val texto1:String = valor1.text.toString()
-            val texto2:String = valor2.text.toString()
-
-            try {
-                numero1 = texto1.toInt()
-                numero2 = texto2.toInt()
-            }
-            catch (e:InputMismatchException){
-                Toast.makeText(this, "Error, Ingrese un Numero", Toast.LENGTH_SHORT).show()
-            }
-
-            resultado = (numero1 - numero2).toFloat()
-
-            val textoresultado = resultado.toString()
-
-            intent.putExtra("resultado", textoresultado)
+            val intent: Intent = Intent(this, Resta::class.java)
 
             startActivityForResult(intent, 1)
         }
 
         btnMultiplicar.setOnClickListener{
-            val intent: Intent = Intent(this, MainActivity2::class.java)
-
-            var numero1: Int = 0
-            var numero2: Int = 0
-            var resultado:Float = 0f
-
-            val valor1:EditText = findViewById(R.id.valor1)
-            val valor2:EditText = findViewById(R.id.valor2)
-
-            val texto1:String = valor1.text.toString()
-            val texto2:String = valor2.text.toString()
-
-            try {
-                numero1 = texto1.toInt()
-                numero2 = texto2.toInt()
-            }
-            catch (e:InputMismatchException){
-                Toast.makeText(this, "Error, Ingrese un Numero", Toast.LENGTH_SHORT).show()
-            }
-
-            resultado = (numero1 * numero2).toFloat()
-
-            val textoresultado = resultado.toString()
-
-            intent.putExtra("resultado", textoresultado)
+            val intent: Intent = Intent(this, Multiplicacion::class.java)
 
             startActivityForResult(intent, 1)
         }
 
         btnDividir.setOnClickListener{
-            val intent: Intent = Intent(this, MainActivity2::class.java)
-
-            var numero1: Int = 0
-            var numero2: Int = 0
-            var resultado:Float = 0f
-
-            val valor1:EditText = findViewById(R.id.valor1)
-            val valor2:EditText = findViewById(R.id.valor2)
-
-            val texto1:String = valor1.text.toString()
-            val texto2:String = valor2.text.toString()
-
-            try {
-                numero1 = texto1.toInt()
-                numero2 = texto2.toInt()
-            }
-            catch (e:InputMismatchException){
-                Toast.makeText(this, "Error, Ingrese un Numero", Toast.LENGTH_SHORT).show()
-            }
-
-            resultado = (numero1 / numero2).toFloat()
-
-            val textoresultado = resultado.toString()
-
-            intent.putExtra("resultado", textoresultado)
+            val intent: Intent = Intent(this, Division::class.java)
 
             startActivityForResult(intent, 1)
         }
@@ -139,12 +40,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode == Activity.RESULT_OK){
-            if(resultCode == 1){
+
+        if(resultCode == RESULT_OK){
+            if(requestCode == 1){
                 val bundle = data?.extras
-                if(bundle != null){
-                    val resultado = bundle.getString("resultado1")
-                    Toast.makeText(this, resultado, Toast.LENGTH_SHORT).show()
+                if(bundle!=null){
+                    val resultado = bundle.getString("resultado")
+                    txtResultado.text = resultado
                 }
             }
         }
